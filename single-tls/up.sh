125c837089e8b7940d49f28056c9b6b32c0575c7
Yaocheng YE <yeyc.sailing@gmail.com>
2017-06-17 17:06:49 +0800

    docker net

--- single-tls/up.sh
+++ single-tls/up.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+node_ip=10.24.237.62
 membersrvc_log_level=DEBUG
 peer_log_level=DEBUG
 
-membersrvc_endpoint=membersrvc:7054
+
+membersrvc_endpoint=$node_ip:7054
 echo "Set MEMBERSRVC to "$membersrvc_endpoint
+rootnode_endpoint=$node_ip:7051
+echo "Set ROOTNODE to "$rootnode_endpoint
 
 case $1 in
-    1)  MEMBERSRVC=$membersrvc_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp.yaml up
+    1)  MEMBERSRVC=$membersrvc_endpoint LOCALIP=$node_ip ROOTNODE=$rootnode_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp.yaml up
     ;;
-    2)  MEMBERSRVC=$membersrvc_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp-2nvp.yaml up
+    2)  MEMBERSRVC=$membersrvc_endpoint LOCALIP=$node_ip ROOTNODE=$rootnode_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp-2nvp.yaml up
     ;;
 esac
