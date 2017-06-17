ec62e109638db370a43d6eb3a25d5d8ed724dbe5
Yaocheng YE <yeyc.sailing@gmail.com>
2017-06-17 16:18:58 +0800

    single-tls-fix

--- single-tls/down.sh
+++ single-tls/down.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker-compose -f membersrvc-4vp.yaml down
+case $1 in
+    1)  docker-compose -f membersrvc-4vp.yaml down
+    ;;
+    2)  docker-compose -f membersrvc-4vp-2nvp.yaml down
+    ;;
+esac
--- single-tls/up.sh
+++ single-tls/up.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-membersrvc_log_level=INFO
-peer_log_level=INFO
+membersrvc_log_level=DEBUG
+peer_log_level=DEBUG
 
 membersrvc_endpoint=membersrvc:7054
 echo "Set MEMBERSRVC to "$membersrvc_endpoint
 
-MEMBERSRVC=$membersrvc_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp.yaml up
+case $1 in
+    1)  MEMBERSRVC=$membersrvc_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp.yaml up
+    ;;
+    2)  MEMBERSRVC=$membersrvc_endpoint MEMBERSRVC_LOGGING=$membersrvc_log_level PEER_LOGGING=$peer_log_level docker-compose -f membersrvc-4vp-2nvp.yaml up
+    ;;
+esac
